e7893aa72df3dd21b55d852a8f6006c967c79a9d
Chrispus <skilochris@gmail.com>
2023-12-11 10:28:38 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,12 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci_sequence = [0, 1]
+    
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:length])
+
+print_fibonacci(9)
